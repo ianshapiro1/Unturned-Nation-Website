460d8d3424ebcd26a3069abc021e569beb9829db
ianshapiro1 <ianshapiro1@gmail.com>
2018-08-17 22:20:20 -0700

    scrollbar

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="main.css">
 <link rel="stylesheet" href="imagehover.css">
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.css">
+<script src="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.js"></script>
 <body>
 <?php
 
