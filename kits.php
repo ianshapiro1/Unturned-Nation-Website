3795d220df60eb58fa47099b02d1cf58327b6d1a
ianshapiro1 <ianshapiro1@gmail.com>
2018-08-20 13:36:58 -0700

    navbar

--- kits.php
+++ kits.php
@@ -5,26 +5,27 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="main.css">
 <link rel="stylesheet" href="imagehover.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.css">
 <script src="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.js"></script>
 <body>
 
 <!-- Navbar -->
 <div class="w3-top" style="opacity: 0.95;">
   <div class="w3-bar w3-opaqueteracotta w3-card w3-left-align w3-xlarge">
-    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-navbar w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a class="w3-bar-item w3-button w3-hide-large w3-right w3-padding-hamburger w3-hover-navbar"href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><img class="w3-right" style="padding-top: 10px; padding-right: 10px;" src="https://recreation.willistonparks.com/images/bars.svg" width="50" height="50"/></a>
+    <a href="cart.php" class="w3-bar-item w3-button w3-right w3-hover-navbar w3-red"><img src="http://i63.tinypic.com/54um11.png" width="45px" height="45px"/></a>
     <a href="index.php" class="w3-bar-item w3-button w3-hover-navbar"><img id="homelogo" src="http://i65.tinypic.com/kbd0s0_th.png"/></a>
     <a href="kits.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Kits</a>
     <a href="ranks.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Ranks</a>
     <a href="perks.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Perks</a>
+    <a href="upgrades.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Upgrades</a>
     <a href="contact.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Contact Us</a>
   </div>
-
-  <!-- Navbar on small screens -->
-  <div id="navDemo" class="w3-bar-block w3-opaqueteracotta w3-text-light-green w3-lemonmilk w3-hide w3-hide-large w3-hide-medium w3-large">
+    <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-bar-block w3-opaqueteracotta w3-text-green4 w3-lemonmilk w3-hide w3-hide-large">
     <a href="kits.php" class="w3-bar-item w3-button w3-padding-large">Kits</a>
     <a href="ranks.php" class="w3-bar-item w3-button w3-padding-large">Ranks</a>
     <a href="perks.php" class="w3-bar-item w3-button w3-padding-large">Perks</a>
+    <a href="upgrades.php" class="w3-bar-item w3-button w3-padding-large">Upgrades</a>
     <a href="contact.php" class="w3-bar-item w3-button w3-padding-large">Contact Us</a>
   </div>
 </div>
@@ -83,6 +84,9 @@ var Alert = new CustomAlert();
               <p class="w3-tiny">*Click for Details*</p>
             </div>
           </button>
+          <form action="cart.php" method="post">
+            <input type="submit"/>
+          </form>
         </figcaption>
       </figure>
     </div>
--- ranks.php
+++ ranks.php
@@ -11,27 +11,27 @@
 
 <body>
 
-<!-- Navbar -->
-<div class="w3-top" style="opacity: 0.95;">
-  <div class="w3-bar w3-opaqueteracotta w3-card w3-left-align w3-xlarge">
-    <a class="w3-bar-item w3-button w3-hide-large w3-right w3-padding-hamburger w3-hover-navbar"href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><img class="w3-right" style="padding-top: 10px; padding-right: 10px;" src="https://recreation.willistonparks.com/images/bars.svg" width="50" height="50"/></a>
-    <a href="index.php" class="w3-bar-item w3-button w3-hover-navbar"><img id="homelogo" src="http://i65.tinypic.com/kbd0s0_th.png"/></a>
-    <a href="kits.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Kits</a>
-    <a href="ranks.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Ranks</a>
-    <a href="perks.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Perks</a>
-    <a href="upgrades.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Upgrades</a>
-    <a href="contact.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Contact Us</a>
+  <!-- Navbar -->
+  <div class="w3-top" style="opacity: 0.95;">
+    <div class="w3-bar w3-opaqueteracotta w3-card w3-left-align w3-xlarge">
+      <a class="w3-bar-item w3-button w3-hide-large w3-right w3-padding-hamburger w3-hover-navbar"href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><img class="w3-right" style="padding-top: 10px; padding-right: 10px;" src="https://recreation.willistonparks.com/images/bars.svg" width="50" height="50"/></a>
+      <a href="cart.php" class="w3-bar-item w3-button w3-right w3-hover-navbar w3-red"><img src="http://i63.tinypic.com/54um11.png" width="45px" height="45px"/></a>
+      <a href="index.php" class="w3-bar-item w3-button w3-hover-navbar"><img id="homelogo" src="http://i65.tinypic.com/kbd0s0_th.png"/></a>
+      <a href="kits.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Kits</a>
+      <a href="ranks.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Ranks</a>
+      <a href="perks.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Perks</a>
+      <a href="upgrades.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Upgrades</a>
+      <a href="contact.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Contact Us</a>
+    </div>
+      <!-- Navbar on small screens -->
+    <div id="navDemo" class="w3-bar-block w3-opaqueteracotta w3-text-green4 w3-lemonmilk w3-hide w3-hide-large">
+      <a href="kits.php" class="w3-bar-item w3-button w3-padding-large">Kits</a>
+      <a href="ranks.php" class="w3-bar-item w3-button w3-padding-large">Ranks</a>
+      <a href="perks.php" class="w3-bar-item w3-button w3-padding-large">Perks</a>
+      <a href="upgrades.php" class="w3-bar-item w3-button w3-padding-large">Upgrades</a>
+      <a href="contact.php" class="w3-bar-item w3-button w3-padding-large">Contact Us</a>
+    </div>
   </div>
-
-  <!-- Navbar on small screens -->
-  <div id="navDemo" class="w3-bar-block w3-opaqueteracotta w3-text-green4 w3-lemonmilk w3-hide w3-hide-large">
-    <a href="kits.php" class="w3-bar-item w3-button w3-padding-large">Kits</a>
-    <a href="ranks.php" class="w3-bar-item w3-button w3-padding-large">Ranks</a>
-    <a href="perks.php" class="w3-bar-item w3-button w3-padding-large">Perks</a>
-    <a href="upgrades.php" class="w3-bar-item w3-button w3-padding-large">Upgrades</a>
-    <a href="contact.php" class="w3-bar-item w3-button w3-padding-large">Contact Us</a>
-  </div>
-</div>
 
 <div class="w3-center" style="margin-top: 50px;padding: 100px; padding-left: 0px; padding-right: 0px;">
   <div class="w3-text-white w3-lemonmilk">
