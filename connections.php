97810efab2256e35031526532f079172bef37505
ianshapiro1 <ianshapiro1@gmail.com>
2018-08-20 16:27:56 -0700

    favicon, navbars, and stripe php

--- connections.php
+++ connections.php
@@ -1,13 +1,15 @@
-<?php
+<?php 
 
    $server="localhost";
    $user="billgates";
    $pass="DuVBm5gevEiNVcyo64V1jXbqfsei20344";
-   $db="unturnednationcart"; 
+   $db="unturnednationcart";
 
+   // connect to mysql
 
    mysql_connect($server, $user, $pass) or die("Sorry, can't connect to the mysql.");
 
+   // select the db
 
    mysql_select_db($db) or die("Sorry, can't select the database.");
 
