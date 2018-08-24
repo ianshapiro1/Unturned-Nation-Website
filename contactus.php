bd1c6769f036e81e4740c92fb2c6485b9653a15a
ianshapiro1 <ianshapiro1@gmail.com>
2018-08-24 00:54:32 -0700

    new files

---
+++ contactus.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html class="w3-bannerimage">
+<head>
+  <title>Unturned Nation</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="imagehover.css">
+  <link rel="icon" type="image/png" href="http://i65.tinypic.com/kbd0s0_th.png" sizes="16x16">
+</head>
+<body>
+<!-- Navbar -->
+<div class="w3-top" style="opacity: 0.95;">
+  <div class="w3-bar w3-opaqueteracotta w3-card w3-left-align w3-navbarfontsize">
+    <a class="w3-bar-item w3-button w3-hide-large w3-right w3-padding-hamburger w3-hover-navbar"href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><img class="w3-right" src="https://preview.ibb.co/hz3J3K/hamburger.png" width="45" height="45"/></a>
+    <form class="w3-bar-item w3-button w3-hover-navbar w3-right" target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
+      <input type="hidden" name="cmd" value="_s-xclick">
+      <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIG1QYJKoZIhvcNAQcEoIIGxjCCBsICAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYA/l+gqV5EG3zULYf3U2lVK2h0k6uJ5DSCmHJqmEuTtRf1tBD93/a0i2uuMQVWb0vCuoIUnvlIbt5z5qAH2/AfSg1boijkxoLMYMNrIwjzTfDnNtehHZwASDJzSFu2pPp4uI8D+MwLayuSX4fIGoycEeeU8sBKhyC5BgnkaI+YXLTELMAkGBSsOAwIaBQAwUwYJKoZIhvcNAQcBMBQGCCqGSIb3DQMHBAiCzJQ9K++8BIAwFbF8vsYbTetpywwuZ5yErTfOdR6W1pVWs1nj5jPVBbhejTcDikCxZLP7MS1el148oIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTgwODIwMjA1MzQ1WjAjBgkqhkiG9w0BCQQxFgQUcfVrsrT03Fu0y9W6aFyR/RIWVMQwDQYJKoZIhvcNAQEBBQAEgYBk2CG+yb6zoShGIOgBvOMJL2I/6+Zcd42R6hJ/N7ttRuD9s+37X1kY5AAwW/HuAkREPo/yvY94e6bzb5d9k6czhaw28t855RXWyBXlsxOu1G5N3vOI2vhSK1IgSW4YU/8oxJp9IS+DGo69vbx+Y8cBxpnNqN+PQ1tA1FKa+x9c3w==-----END PKCS7-----
+      ">
+      <input type="image" src="http://s33.postimg.cc/p97a2yacr/REALcarticon.png" width="39px" height="39px" name="submit" alt="Paypal Cart"/>
+    </form>
+    <a href="index.php" class="w3-bar-item w3-button w3-hover-navbar"><img id="homelogo" src="http://i65.tinypic.com/kbd0s0_th.png"/></a>
+    <a href="kits.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Kits</a>
+    <a href="ranks.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Ranks</a>
+    <a href="perks.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Perks</a>
+    <a href="upgrades.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Upgrades</a>
+    <a href="contact.php" class="w3-bar-item w3-button w3-lemonmilk w3-hide-small w3-padding-large w3-hover-navbar w3-hover-text-green4 w3-text-green4">Contact Us</a>
+  </div>
+
+  <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-bar-block w3-opaqueteracotta w3-text-green4 w3-lemonmilk w3-hide w3-hide-large">
+    <a href="kits.php" class="w3-bar-item w3-button w3-padding-large">Kits</a>
+    <a href="ranks.php" class="w3-bar-item w3-button w3-padding-large">Ranks</a>
+    <a href="perks.php" class="w3-bar-item w3-button w3-padding-large">Perks</a>
+    <a href="upgrades.php" class="w3-bar-item w3-button w3-padding-large">Upgrades</a>
+    <a href="contact.php" class="w3-bar-item w3-button w3-padding-large">Contact Us</a>
+  </div>
+</div>
+
+<!-- Header -->
+  <header class="w3-container" id="header" >
+    <div class="bg-header">
+      <img class="responsive" src="http://oi64.tinypic.com/14cgx2p.jpg"/>
+    </div>
+  </header>
+
+<!-- First Grid -->
+<div class="w3-row-padding w3-padding-32 w3-container w3-green4opq">
+  <div class="w3-content">
+    <div class="w3-third pad20 w3-right">
+      <div>
+        <img src="http://oi68.tinypic.com/2yll7gm.jpg" class="responsive washingtonborder2"/>
+        <div class="carpattext w3-lemonmilk text-dark-gray">Carpat: not.a.real.ip</div>
+      </div>
+      <div>
+        <img src="http://oi63.tinypic.com/2uidy03.jpg" class="responsive washingtonborder2"/>
+        <div class="washingtontext w3-lemonmilk text-dark-gray">PEI: not.a.real.ip</div>
+      </div>
+      <div>
+        <img src="http://oi65.tinypic.com/6ycd1g.jpg" class="responsive washingtonborder2"/>
+        <div class="peitext w3-lemonmilk text-dark-gray">Washington: not.a.real.ip</div>
+      </div>
+    </div>
+    <div class="w3-twothird pad20">
+      <div class="w3-panel" style="margin: 0">
+        <h1 class="w3-lemonmilk w3-text-brown w3-xxxlarge underline">Our Servers</h1>
+      </div>
+      <h5 class="w3-dark text-dark-gray w3-xlarge w3-liberation" style="margin: 0;">Unturned Nation hosts PVP Unturned servers <br> with Ranks, Hand-Made Kits, and Commands.</h5>
+      <h3 class="w3-text-brown w3-xxlarge w3-lemonmilk">Features and Plugins:</h3>
+      <ul>
+        <li class="text-dark-gray w3-xlarge w3-liberation">11 FREE Custom-made Kits</li>
+        <li class="text-dark-gray w3-xlarge w3-liberation">/Home</li>
+        <li class="text-dark-gray w3-xlarge w3-liberation">/TPA</li>
+        <li class="text-dark-gray w3-xlarge w3-liberation">Feasts</li>
+        <li class="text-dark-gray w3-xlarge w3-liberation">Private Messages</li>
+        <li class="text-dark-gray w3-xlarge w3-liberation">Uconomy</li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<!-- Second Grid -->
+<div class="w3-row-padding w3-opaqueteracotta w3-container"id="SecondBlock">
+  <div class="w3-content">
+    <div class="w3-twothird w3-text-green4">
+      <div class="pad20">
+        <h1 class="w3-lemonmilk w3-xxxlarge underline">Rules & Terms of Service</h1>
+        <h5 class="w3-padding-32 text-dark-gray w3-liberation w3-xlarge">Players that join our servers will be held responsible for all<br>actions that occur on their account whether or not the<br>Rules and Terms of Service are read or understood.</h5>
+
+        <p class="w3-text-green4 w3-liberation w3-large">•  We believe in freedom of speech, so our servers allow for all languages<br>and vocabulary. The only exceptions to this rule are threats, doxing,<br>defamation (libel & slander), or any other communicative event that could<br>be pursued in a court of law. Not abiding by this rule will result in a<br>permanent ban, without a warning, across all servers.</p>
+        <p class="w3-text-green4 w3-liberation w3-large">•  Hacking, third party programs, or game modification of any kind is not<br>tolerated and will result in a permanent ban, without a warning. Glitches,<br>however, are permissible.</p>
+        <p class="w3-text-green4 w3-liberation w3-large">•  Advertisements without owner permission and spam are not allowed on<br>any server and will result in an immediate kick, without a warning. Second<br>offenses will be permanently banned, without a warning.</p>
+        <p class="w3-text-green4 w3-liberation w3-large">•  Please refrain from playing music at a level that is audible to other<br>players from your microphone. Doing this will result in a kick, without a<br>warning.</p>
+        <p class="w3-text-green4 w3-liberation w3-large">•  Structures that cause game lag are subject to removal. Although there is<br>no penalty to this situation, items that have been removed will not be<br>replaced.</p>
+        <p class="text-dark-gray w3-liberation w3-xlarge">  Only admins have black chat text. If someone states they<br>are an admin/have admin permissions and do not write in<br>black text, they are not an admin. If you wish to report<br>another player for breaking any rules, please provide proof<br>and fill out <a href="#">this form</a>.</p>
+      </div>
+    </div>
+    <div class="w3-third w3-center">
+      <div class="pad20">
+        <h3 class="w3-text-green4 w3-liberation">Newsletter</h3>
+           <p class="text-dark-gray w3-liberation leftalign">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut eveniet non quaerat, reprehenderit assumenda incidunt tenetur asperiores recusandae eos autem. Dicta et consequatur nobis rem impedit iure illum dolorem quis?</p>
+           <p class="text-dark-gray w3-liberation leftalign">Sequi totam velit ducimus illo, qui expedita sapiente. Eaque dolore harum quisquam voluptatem sit doloribus cum. Neque, dolorum quos est consequuntur error tenetur, quod ipsa sit eaque doloremque deserunt! Voluptates.</p>
+           <p class="text-dark-gray w3-liberation leftalign">Omnis nostrum, vel recusandae voluptates eius at temporibus iusto neque, odit quasi obcaecati tempore consectetur corrupti hic. Adipisci eaque culpa obcaecati reiciendis fugiat cupiditate id aliquid, quos sed debitis quisquam!</p>
+           <p class="text-dark-gray w3-liberation leftalign">Numquam corporis blanditiis in ullam accusamus sed error fugit consequuntur quam. Minus facilis commodi consectetur asperiores maxime facere blanditiis modi nihil eos temporibus rerum odit quam quasi optio, sapiente molestiae?</p>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center ">
+  <div class="w3-large w3-padding-32 w3-lemonmilk w3-text-green4">
+    <p>&copy; Copyright 2018 Unturned Nation</p>
+ </div>
+</footer>
+
+<script>
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+</script>
+
+</body>
+</html>
